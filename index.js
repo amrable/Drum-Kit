d5295f72d72922458d2fbe1d12fe58bf84bd977b
amrfahmy <amrfahmy57@gmail.com>
2019-05-16 18:11:03 +0200

    adding key listeners

--- index.js
+++ index.js
@@ -4,26 +4,59 @@ for ( var i=0 ; i<len ; i++)
 {
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
 
-    var sound="sounds/";
-    if(this.innerHTML=="w")
-    {
-      sound+="tom-1.mp3";
-    }else if (this.innerHTML=="a") {
-      sound+="tom-2.mp3";
-    }else if(this.innerHTML=="s"){
-      sound+="tom-3.mp3";
-    }else if (this.innerHTML=="d") {
-      sound+="tom-4.mp3";
-    }else if (this.innerHTML=="j") {
-      sound+="crash.mp3";
-    }else if (this.innerHTML=="k") {
-      sound+="snare.mp3";
-    }else if (this.innerHTML=="l") {
-      sound+="kick-bass.mp3";
-    }
-
-
-    var audio=new Audio ( sound );
-    audio.play();
+    var key=this.innerHTML;
+    makeSound(key);
+    keyAnimation(key);
   });
+}
+
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+  keyAnimation(event.key);
+});
+
+
+
+function makeSound(key){
+  switch(key){
+    case "w":
+    var tom1=new Audio ( "sounds/tom-1.mp3" );
+    tom1.play();
+    break;
+    case "a":
+    var tom2=new Audio ( "sounds/tom-2.mp3" );
+    tom2.play();
+    break;
+    case "s":
+    var tom3=new Audio ( "sounds/tom-3.mp3" );
+    tom3.play();
+    break;
+    case "d":
+    var tom4=new Audio ( "sounds/tom-4.mp3" );
+    tom4.play();
+    break;
+    case "j":
+    var crash=new Audio ( "sounds/crash.mp3" );
+    crash.play();
+    break;
+    case "k":
+    var snare=new Audio ( "sounds/snare.mp3" );
+    snare.play();
+    break;
+    case "l":
+    var kick=new Audio ( "sounds/kick-bass.mp3" );
+    kick.play();
+    break;
+
+  }
+}
+
+function keyAnimation(key){
+
+  document.querySelector("."+key).classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector("."+key).classList.remove("pressed");
+
+  }, 100);
+
 }
